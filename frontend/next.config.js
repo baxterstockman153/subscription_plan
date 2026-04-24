dd265d3303ef1b102f43dd9f2a663bb290f900b9
Tom Bombadil <mark.huber153@gmail.com>
2026-04-23 21:45:01 -0700

    fix config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [{ source: '/api/:path*', destination: 'http://localhost:3000/api/:path*' }];
+    return [{ source: '/api/:path*', destination: `${process.env.BACKEND_URL}/api/:path*` }];
   },
 };
 module.exports = nextConfig;
